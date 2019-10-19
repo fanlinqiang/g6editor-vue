9278e7769b8729babe0b8b2d08e92d16c5f82467
fanlinqiang <linqiang.fan@yiducloud.cn>
2019-10-19 15:20:22 +0800

    modify vue-config outputDir docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // // 强制4个空格缩进
+        'indent': [2, 4, {'SwitchCase': 1}]
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 }
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // build 时生成的生产环境构建文件的目录。
+    outputDir: 'docs',
+    // 生产环境的 source map
+    productionSourceMap: false
+}
