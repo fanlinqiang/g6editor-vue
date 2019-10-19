a914b8550e0c55d269b6bc33bb298791f61d67fb
fanlinqiang <linqiang.fan@yiducloud.cn>
2019-10-19 15:35:12 +0800

    modify vue-config publicPath

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,11 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         // // 强制4个空格缩进
-        'indent': [2, 4, {'SwitchCase': 1}]
+        'indent': [2, 4, { 'SwitchCase': 1 }],
+        // // 行尾必须有分号
+        'semi': ['error', 'always']
     },
     parserOptions: {
         parser: 'babel-eslint'
     }
-}
+};
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '././' : '/';
 module.exports = {
     // 部署应用包时的基本 URL
-    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+    publicPath,
     // build 时生成的生产环境构建文件的目录。
     outputDir: 'docs',
     // 生产环境的 source map
     productionSourceMap: false
-}
+};
